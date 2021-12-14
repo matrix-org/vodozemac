f1bcb0b367d5040e90c604db3169fa0b7261ca73
Damir Jelić <poljar@termina.org.uk>
2021-12-14 11:39:04 +0100

    refactor(cipher): Split the cipher module into separate submodules

--- src/cipher/mod.rs
+++ src/cipher/mod.rs
@@ -1,51 +1,13 @@
-use aes::{cipher::generic_array::GenericArray, Aes256, NewBlockCipher};
+mod key;
+
+use aes::Aes256;
 use block_modes::{block_padding::Pkcs7, BlockMode, BlockModeError, Cbc};
-use hkdf::Hkdf;
 use hmac::{digest::MacError, Hmac, Mac as MacT};
+use key::CipherKeys;
 use sha2::Sha256;
-use zeroize::Zeroize;
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 type HmacSha256 = Hmac<Sha256>;
-
-type Aes256Key = GenericArray<u8, <Aes256 as NewBlockCipher>::KeySize>;
-type Aes256Iv = GenericArray<u8, <Aes256Cbc as BlockMode<Aes256, Pkcs7>>::IvSize>;
-type HmacSha256Key = [u8; 32];
-
-#[derive(Clone, Zeroize)]
-struct ExpandedKeys([u8; 80]);
-
-impl Drop for ExpandedKeys {
-    fn drop(&mut self) {
-        self.0.zeroize();
-    }
-}
-
-impl ExpandedKeys {
-    const HMAC_INFO: &'static [u8] = b"OLM_KEYS";
-
-    fn new(message_key: &[u8; 32]) -> Self {
-        let mut expanded_keys = [0u8; 80];
-
-        let hkdf: Hkdf<Sha256> = Hkdf::new(Some(&[0]), message_key);
-
-        hkdf.expand(Self::HMAC_INFO, &mut expanded_keys).expect("Can't expand message key");
-
-        Self(expanded_keys)
-    }
-
-    fn split(self) -> ([u8; 32], [u8; 16], [u8; 32]) {
-        let mut aes_key = [0u8; 32];
-        let mut mac_key = [0u8; 32];
-        let mut aes_iv = [0u8; 16];
-
-        aes_key.copy_from_slice(&self.0[0..32]);
-        mac_key.copy_from_slice(&self.0[32..64]);
-        aes_iv.copy_from_slice(&self.0[64..80]);
-
-        (aes_key, aes_iv, mac_key)
-    }
-}
 
 pub(crate) struct Mac([u8; 32]);
 
@@ -57,42 +19,6 @@ impl Mac {
         truncated.copy_from_slice(&self.0[0..Self::TRUNCATED_LEN]);
 
         truncated
-    }
-}
-
-#[derive(Clone, Zeroize)]
-pub(super) struct CipherKeys {
-    aes_key: [u8; 32],
-    aes_iv: [u8; 16],
-    mac_key: [u8; 32],
-}
-
-impl Drop for CipherKeys {
-    fn drop(&mut self) {
-        self.aes_key.zeroize();
-        self.aes_iv.zeroize();
-        self.mac_key.zeroize();
-    }
-}
-
-impl CipherKeys {
-    pub fn new(message_key: &[u8; 32]) -> Self {
-        let expanded_key = ExpandedKeys::new(message_key);
-        let (aes_key, aes_iv, mac_key) = expanded_key.split();
-
-        Self { aes_key, aes_iv, mac_key }
-    }
-
-    fn aes_key(&self) -> &Aes256Key {
-        Aes256Key::from_slice(&self.aes_key)
-    }
-
-    fn mac_key(&self) -> &HmacSha256Key {
-        &self.mac_key
-    }
-
-    fn iv(&self) -> &Aes256Iv {
-        Aes256Iv::from_slice(&self.aes_iv)
     }
 }
 
