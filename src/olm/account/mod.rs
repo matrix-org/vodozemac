9612f3d7ad9b785d84db970c10a0d659f9b4ff16
Denis Kasak <dkasak@termina.org.uk>
2022-01-13 11:37:12 +0000

    Make doc comment a bit more explicit regarding use.

--- src/olm/account/mod.rs
+++ src/olm/account/mod.rs
@@ -203,10 +203,13 @@ impl Account {
             .or_else(|| self.fallback_keys.get_secret_key(public_key))
     }
 
-    /// Remove a one time key that has previously been published.
+    /// Remove a one-time key that has previously been published but not yet
+    /// used.
     ///
-    /// **Note:** You do not need to manually call this after a one time key has
-    /// been used, as that is handled by [`Account::create_inbound_session`].
+    /// **Note**: This function is only rarely useful and you'll know if you
+    /// need it. Notably, you do *not* need to call it manually when using up
+    /// a key via [`Account::create_inbound_session`] since the key is
+    /// automatically removed in that case.
     pub fn remove_one_time_key(
         &mut self,
         public_key: &Curve25519PublicKey,
