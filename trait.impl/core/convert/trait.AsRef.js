a826e30ab97a3b8bc44153895716f908d1e9d67d
poljar <poljar@users.noreply.github.com>
2025-01-31 09:17:54 +0000

    deploy: 614d59a121200bbf1cf12858332ce203e9b1edbd

---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"vodozemac/struct.Curve25519PublicKey.html\" title=\"struct vodozemac::Curve25519PublicKey\">Curve25519PublicKey</a>&gt; for <a class=\"struct\" href=\"vodozemac/olm/struct.RatchetPublicKey.html\" title=\"struct vodozemac::olm::RatchetPublicKey\">RatchetPublicKey</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[460]}
