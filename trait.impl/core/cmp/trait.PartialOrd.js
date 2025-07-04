e0d7c273193ab61b28e1a42f8e4506752a992ab7
poljar <poljar@users.noreply.github.com>
2025-07-04 11:01:55 +0000

    deploy: bcc57c1644ee1f42b06f7216c514861cdd35b4ca

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vodozemac/struct.KeyId.html\" title=\"struct vodozemac::KeyId\">KeyId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[275]}
---
+++ vodozemac/ecies/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error","MessageDecodeError"],"struct":["CheckCode","Ecies","EstablishedEcies","InboundCreationResult","InitialMessage","Message","OutboundCreationResult"]};
