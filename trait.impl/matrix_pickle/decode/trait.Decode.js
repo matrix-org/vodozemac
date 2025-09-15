ce20b5eccc4735f3e0c6bc87b6b2697907d6aaef
poljar <poljar@users.noreply.github.com>
2025-09-15 12:45:34 +0000

    deploy: 5ebd33abdda9df216401a942e4c3db176896ac61

---
+++ trait.impl/matrix_pickle/decode/trait.Decode.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://docs.rs/matrix-pickle/0.2.1/matrix_pickle/decode/trait.Decode.html\" title=\"trait matrix_pickle::decode::Decode\">Decode</a> for <a class=\"struct\" href=\"vodozemac/olm/struct.SessionKeys.html\" title=\"struct vodozemac::olm::SessionKeys\">SessionKeys</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/matrix-pickle/0.2.1/matrix_pickle/decode/trait.Decode.html\" title=\"trait matrix_pickle::decode::Decode\">Decode</a> for <a class=\"struct\" href=\"vodozemac/struct.Curve25519PublicKey.html\" title=\"struct vodozemac::Curve25519PublicKey\">Curve25519PublicKey</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[632]}
