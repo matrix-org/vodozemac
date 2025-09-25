1125497b3d065a72ad5af780a8111541620f1124
poljar <poljar@users.noreply.github.com>
2025-09-25 09:02:45 +0000

    deploy: df503800eca7d38333735f00d1c5e13c283d125b

---
+++ trait.impl/zeroize/trait.Zeroize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://docs.rs/zeroize/1.8.1/zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"vodozemac/ecies/struct.EstablishedEcies.html\" title=\"struct vodozemac::ecies::EstablishedEcies\">EstablishedEcies</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/zeroize/1.8.1/zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"vodozemac/megolm/struct.ExportedSessionKey.html\" title=\"struct vodozemac::megolm::ExportedSessionKey\">ExportedSessionKey</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/zeroize/1.8.1/zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"vodozemac/megolm/struct.SessionKey.html\" title=\"struct vodozemac::megolm::SessionKey\">SessionKey</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[877]}
