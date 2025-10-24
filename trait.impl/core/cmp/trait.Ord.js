65a252c5b919e764764209b72dba9966a002504f
poljar <poljar@users.noreply.github.com>
2025-10-24 10:09:38 +0000

    deploy: f47cd0d6b935d7b53158dcef9961a11bc4c7d3d2

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"vodozemac/struct.KeyId.html\" title=\"struct vodozemac::KeyId\">KeyId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[254]}
---
+++ vodozemac/olm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DecryptionError","MessageType","OlmMessage","SessionCreationError"],"struct":["Account","AccountPickle","IdentityKeys","InboundCreationResult","Message","OneTimeKeyGenerationResult","PreKeyMessage","RatchetPublicKey","Session","SessionConfig","SessionKeys","SessionPickle"]};
