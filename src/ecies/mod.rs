3cfe13f527d0b6f0d919c3bcb33028883249f767
Damir Jelić <poljar@termina.org.uk>
2026-01-14 15:48:22 +0100

    chore: Silence a warning about unused assignments

--- src/ecies/mod.rs
+++ src/ecies/mod.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(unused_assignments)]
+
 //! Implementation of an integrated encryption scheme.
 //!
 //! This module implements
