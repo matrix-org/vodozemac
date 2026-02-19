6f9aecc8abefbd45f7b4dbfd63680a36ec931469
poljar <poljar@users.noreply.github.com>
2026-02-19 13:21:49 +0000

    deploy: ee3c9874401b0266c591a20c2dbaf2fb02f96ea7

---
+++ trait.impl/matrix_pickle/decode/trait.Decode.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://docs.rs/matrix-pickle/0.2.2/matrix_pickle/decode/trait.Decode.html\" title=\"trait matrix_pickle::decode::Decode\">Decode</a> for <a class=\"struct\" href=\"vodozemac/olm/struct.SessionKeys.html\" title=\"struct vodozemac::olm::SessionKeys\">SessionKeys</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/matrix-pickle/0.2.2/matrix_pickle/decode/trait.Decode.html\" title=\"trait matrix_pickle::decode::Decode\">Decode</a> for <a class=\"struct\" href=\"vodozemac/struct.Curve25519PublicKey.html\" title=\"struct vodozemac::Curve25519PublicKey\">Curve25519PublicKey</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[636]}
---
+++ vodozemac/megolm/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DecryptionError","SessionKeyDecodeError","SessionOrdering"],"struct":["DecryptedMessage","ExportedSessionKey","GroupSession","GroupSessionPickle","InboundGroupSession","InboundGroupSessionPickle","MegolmMessage","SessionConfig","SessionKey"]};
