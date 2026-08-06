a1ea87af90fe9a4c3cbce808ca06d99d58f79740
poljar <poljar@users.noreply.github.com>
2026-08-06 10:15:21 +0000

    deploy: cfc4eeebdf0953314144a6b709449b0434019ca2

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vodozemac",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"vodozemac/ecies/struct.EstablishedEcies.html\" title=\"struct vodozemac::ecies::EstablishedEcies\">EstablishedEcies</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"vodozemac/megolm/struct.ExportedSessionKey.html\" title=\"struct vodozemac::megolm::ExportedSessionKey\">ExportedSessionKey</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"vodozemac/megolm/struct.SessionKey.html\" title=\"struct vodozemac::megolm::SessionKey\">SessionKey</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[910]}
