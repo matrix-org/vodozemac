9888574c27f8d736c0ef118e1d65cde436fd6a74
Damir Jelić <poljar@termina.org.uk>
2026-08-25 15:54:23 +0200

    Mark some of the indices in olm/megolm messages as required
    This ensures that the fields won't be omitted if they contain the
    default value 0.
    
    This removes the need for our custom encode_manual methods while
    retaining libolm compatibility.
    
    Took a while before coming back to this.

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -22,6 +22,7 @@ use base64::{
 pub(crate) use libolm_compat::get_version as get_pickle_version;
 #[cfg(feature = "libolm-compat")]
 pub(crate) use libolm_compat::{LibolmEd25519Keypair, pickle_libolm, unpickle_libolm};
+use prost::Message;
 
 const STANDARD_NO_PAD: GeneralPurpose = GeneralPurpose::new(
     &alphabet::STANDARD,
@@ -89,74 +90,26 @@ pub(crate) fn extract_mac(slice: &[u8], truncated: bool) -> crate::cipher::Messa
     }
 }
 
-// The integer encoding logic here has been taken from the integer-encoding[1]
-// crate and is under the MIT license.
-//
-// The MIT License (MIT)
-//
-// Copyright (c) 2016 Google Inc. (lewinb@google.com) -- though not an official
-// Google product or in any way related!
-// Copyright (c) 2018-2020 Lewin Bormann (lbo@spheniscida.de)
-//
-// [1]: https://github.com/dermesser/integer-encoding-rs
-pub(crate) trait VarInt {
-    fn to_var_int(self) -> Vec<u8>;
-}
+/// Generic method to encode a [`Message`] into a newly allocated buffer.
+///
+/// We're not using [Message::encode_to_vec] directly because we want to prepend
+/// a version to our protobuf encoded message. Likewise, when we decode, we
+/// first want to look at the first byte for the version before we decide how to
+/// decode the message.
+pub(crate) fn encode_protobuf_message(message: impl Message, message_version: u8) -> Vec<u8> {
+    let mut output: Vec<u8> = vec![0u8; message.encoded_len() + 1];
+    output[0] = message_version;
 
-/// Most-significant byte, == 0x80
-const MSB: u8 = 0b1000_0000;
+    #[allow(clippy::expect_used)]
+    message
+        .encode(&mut output[1..].as_mut())
+        .expect("We should be able to encode a message into protobuf.");
 
-/// How many bytes an integer uses when being encoded as a VarInt.
-#[inline]
-const fn required_encoded_space_unsigned(mut v: u64) -> usize {
-    if v == 0 {
-        return 1;
-    }
-
-    let mut logcounter = 0;
-    while v > 0 {
-        logcounter += 1;
-        v >>= 7;
-    }
-    logcounter
-}
-
-impl VarInt for usize {
-    fn to_var_int(self) -> Vec<u8> {
-        (self as u64).to_var_int()
-    }
-}
-
-impl VarInt for u32 {
-    fn to_var_int(self) -> Vec<u8> {
-        (self as u64).to_var_int()
-    }
-}
-
-impl VarInt for u64 {
-    #[inline]
-    fn to_var_int(self) -> Vec<u8> {
-        let mut v = vec![0u8; required_encoded_space_unsigned(self)];
-
-        let mut n = self;
-        let mut i = 0;
-
-        while n >= 0x80 {
-            v[i] = MSB | (n as u8);
-            i += 1;
-            n >>= 7;
-        }
-
-        v[i] = n as u8;
-
-        v
-    }
+    output
 }
 
 #[cfg(test)]
 mod test {
-    use ntest::timeout;
-
     use super::*;
 
     #[test]
@@ -172,18 +125,5 @@ mod test {
             first, second,
             "Decoding the same base64 string with and without padding should produce the same result"
         )
-    }
-
-    #[test]
-    #[timeout(10)]
-    fn integer_encoding_required_space() {
-        assert_eq!(required_encoded_space_unsigned(0), 1);
-        assert_eq!(required_encoded_space_unsigned(100), 1);
-        assert_eq!(required_encoded_space_unsigned(1000), 2);
-        assert_eq!(required_encoded_space_unsigned(10000), 2);
-        assert_eq!(required_encoded_space_unsigned(100000), 3);
-        assert_eq!(required_encoded_space_unsigned(1000000), 3);
-        assert_eq!(required_encoded_space_unsigned(10000000), 4);
-        assert_eq!(required_encoded_space_unsigned(1000000000), 5);
     }
 }
